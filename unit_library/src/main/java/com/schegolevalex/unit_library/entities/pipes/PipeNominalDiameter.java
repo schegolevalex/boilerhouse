e55780ac63b5e1243aa145b8620816edf39596e7
Alexey Schegolev <schegolevalex@gmail.com>
2022-09-19 19:53:04 +0300

    add pipeNominalDiameter

---
+++ unit_library/src/main/java/com/schegolevalex/unit_library/entities/pipes/PipeNominalDiameter.java
@@ -0,0 +1,29 @@
+package com.schegolevalex.unit_library.entities.pipes;
+
+public enum PipeNominalDiameter {
+    DN8,
+    DN10,
+    DN15,
+    DN20,
+    DN25,
+    DN32,
+    DN40,
+    DN50,
+    DN65,
+    DN80,
+    DN100,
+    DN125,
+    DN150,
+    DN200,
+    DN250,
+    DN300,
+    DN350,
+    DN400,
+    DN450,
+    DN500,
+    DN600,
+    DN700,
+    DN800,
+    DN900,
+    DN1000
+}
