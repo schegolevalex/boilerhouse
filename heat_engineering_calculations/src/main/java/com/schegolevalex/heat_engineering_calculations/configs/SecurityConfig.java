1a2b76beda732239c166e09bb63ca564f684acac
Alexey Schegolev <schegolevalex@gmail.com>
2022-11-02 11:58:23 +0300

    fix SecurityConfig which implemented deprecated interface

--- heat_engineering_calculations/src/main/java/com/schegolevalex/heat_engineering_calculations/configs/SecurityConfig.java
+++ heat_engineering_calculations/src/main/java/com/schegolevalex/heat_engineering_calculations/configs/SecurityConfig.java
@@ -2,62 +2,34 @@ package com.schegolevalex.heat_engineering_calculations.configs;
 
 import com.schegolevalex.heat_engineering_calculations.security.JWTAccessFilter;
 import com.schegolevalex.heat_engineering_calculations.security.JWTRefreshFilter;
-import com.schegolevalex.heat_engineering_calculations.security.UserDetailsServiceImpl;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
+@Configuration
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    final UserDetailsServiceImpl userDetailsServiceImpl;
     final JWTAccessFilter jwtAccessFilter;
     final JWTRefreshFilter jwtRefreshFilter;
 
     @Autowired
-    public SecurityConfig(UserDetailsServiceImpl userDetailsServiceImpl,
-                          JWTAccessFilter jwtAccessFilter,
+    public SecurityConfig(JWTAccessFilter jwtAccessFilter,
                           JWTRefreshFilter jwtRefreshFilter) {
-        this.userDetailsServiceImpl = userDetailsServiceImpl;
         this.jwtAccessFilter = jwtAccessFilter;
         this.jwtRefreshFilter = jwtRefreshFilter;
-    }
-
-    //todo переписать под не-deprecated
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf().disable()
-//                .
-//    }
-//
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsServiceImpl).passwordEncoder(passwordEncoder());
-    }
-
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/auth/login", "/auth/registration", "/auth/refresh").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .addFilterBefore(jwtRefreshFilter, UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(jwtAccessFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
@@ -66,8 +38,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    @Override
-    public AuthenticationManager authenticationManager() throws Exception {
-        return super.authenticationManager();
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfiguration) throws Exception {
+        return authConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/auth/registration", "/auth/refresh").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(jwtRefreshFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtAccessFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 }
