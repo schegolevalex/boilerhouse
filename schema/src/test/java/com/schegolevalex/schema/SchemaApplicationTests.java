501d944861308a2f27e0790d84686564c1c8bcac
Alexey Schegolev <schegolevalex@gmail.com>
2022-11-04 14:46:00 +0300

    add new schema module

---
+++ schema/src/test/java/com/schegolevalex/schema/SchemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schegolevalex.schema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
